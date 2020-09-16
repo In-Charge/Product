9c00b7891b6d203fbbb3539d146ba8c4f6084c5f
jakubbober <jakubbober.work@gmail.com>
2020-09-16 17:29:29 +0200

    Implement random token generation and adjust date format

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import pytz
+from secrets import token_hex
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -12,7 +12,8 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.now())
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    access_token = db.Column(db.String(200), default='bebebe')
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -22,8 +23,7 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
+        new_task = Todo(content=task_content, access_token=token_hex(16))
 
         db.session.add(new_task)
         db.session.commit()
